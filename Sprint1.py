3851253f6100c83ad748e7692888ed59ab8fb65a
iamme1234567 <iamme930910@gmail.com>
2018-02-21 21:38:32 -0500

    Add files via upload

--- Sprint1.py
+++ Sprint1.py
@@ -4,113 +4,73 @@ Created on Mon Feb 19 10:33:20 2018
 
 @author: yujiezhong
 """
-def CheckDate(date):
-    day30 = ['04','06','09','11']
-    day31 = ['01','03','05','07','08','10','12']
-    if len(date) == 8:
-        if int(date) <= 20180219:
-            if date[4:6] in day30:
-                if int(date[6:])<=30:
-                    return True
-                else:
-                    return False
-            elif date[4:6] in day31:
-                if int(date[6:])<=31:
-                    return True
-                else:
-                    return False
-            elif date[4:6] == "02":
-                if int(date[6:])<=28:
-                    return True
-                else:
-                    return False
-            else:
-                return False
+import datetime
+
+
+def check_date(date):
+    try:
+        if datetime.datetime.strptime(date,"%Y-%m-%d") > datetime.datetime.now():
+            print(date,"is an impossible date!")
+            return False
+    except ValueError:
+        print(date,"is an impossible date!")
+        return False
+    return True
+
+
+def Birth_Before_Parent_Death(cbirth,hdeath,wdeath):
+    #children birth year, month and day
+    if cbirth != "NA":
+        cbirthyr = int(cbirth.split("-")[0])
+        cbirthmon = int(cbirth.split("-")[1])
+        cbirthday = int(cbirth.split("-")[2])
+        if check_date(cbirth)==True:
+            cbirthdate = datetime.date(cbirthyr,cbirthmon,cbirthday)
         else:
             return False
-    else:
-        return False
+            
+    #husband death year, month and day
+    if hdeath != "NA":
+        hdeathyr = int(hdeath.split("-")[0])
+        hdeathmon = int(hdeath.split("-")[1])
+        hdeathday = int(hdeath.split("-")[2])
+        if check_date(hdeath)==True:
+            hdeathdate = datetime.date(hdeathyr,hdeathmon,hdeathday)
+        else:
+            return False
+            
+    #wife death year, month and day
+    if wdeath != "NA":
+        wdeathyr = int(wdeath.split("-")[0])
+        wdeathmon = int(wdeath.split("-")[1])
+        wdeathday = int(wdeath.split("-")[2])
+        if check_date(wdeath)==True:
+            wdeathdate = datetime.date(wdeathyr,wdeathmon,wdeathday)
+        else:
+            return False
 
-
-def CmprDate(cbirth,hdeath,wdeath):
-    cbirthdate = "".join(cbirth.split("-"))# Children's birthday
-    hdeathdate = "".join(hdeath.split("-"))# Husband death date
-    wdeathdate = "".join(wdeath.split("-"))# Wife death date
-
+    #compare dates
     if (hdeath == "NA" and wdeath == "NA"):
-        if CheckDate(cbirthdate)==True:
-            return True
-        else:
-            return False
+        return True
     elif (hdeath == "NA" and wdeath != "NA"):
-        if CheckDate(cbirthdate)==True and CheckDate(wdeathdate)==True:
-            if int(cbirthdate) <= int(wdeathdate):
-                return True
-            else:
-                return False
-        else: 
-            return False
+        if (wdeathdate-cbirthdate).days>=0:
+            return True
     elif (hdeath != "NA" and wdeath == "NA"):
-        if CheckDate(cbirthdate)==True and CheckDate(hdeathdate)==True:
-            if int(hdeathdate[:4]) > int(cbirthdate[:4]):
-                return True
-            elif hdeathdate[:4] == cbirthdate[:4]: #if husband death year equals to child birth year
-                if int(cbirthdate[4:6])-int(hdeathdate[4:6])<9:
-                    return True
-                elif int(cbirthdate[4:6])-int(hdeathdate[4:6])==9:
-                    if int(cbirthdate[6:])-int(hdeathdate[6:])<=0:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            elif int(hdeathdate[:4])+1 == int(cbirthdate[:4]): #if husband death year is 1 year earlier than child birth year
-                if 12-int(hdeathdate[4:6])+int(cbirthdate[4:6])<9:
-                    return True
-                elif 12-int(hdeathdate[4:6])+int(cbirthdate[4:6])==9:
-                    if int(cbirthdate[6:])-int(hdeathdate[6:])<=0:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
+        if (cbirthdate-hdeathdate).days<=270:
+            return True
     elif (hdeath != "NA" and wdeath != "NA"):
-        if CheckDate(cbirthdate)==True and CheckDate(hdeathdate)==True and CheckDate(wdeathdate)==True:
-            if int(cbirthdate) <= int(wdeathdate):
-                if int(hdeathdate[:4]) > int(cbirthdate[:4]):
-                    return True
-                elif hdeathdate[:4] == cbirthdate[:4]: 
-                    if int(cbirthdate[4:6])-int(hdeathdate[4:6])<9:
-                        return True
-                    elif int(cbirthdate[4:6])-int(hdeathdate[4:6])==9:
-                        if int(cbirthdate[6:])-int(hdeathdate[6:])<=0:
-                            return True
-                        else:
-                            return False
-                    else:
-                        return False
-                elif int(hdeathdate[:4])+1 == int(cbirthdate[:4]):
-                    if 12-int(hdeathdate[4:6])+int(cbirthdate[4:6])<9:
-                        return True
-                    elif 12-int(hdeathdate[4:6])+int(cbirthdate[4:6])==9:
-                        if int(cbirthdate[6:])-int(hdeathdate[6:])<=0:
-                            return True
-                        else:
-                            return False
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else: 
-            return False
+        if (wdeathdate-cbirthdate).days>=0 and (cbirthdate-hdeathdate).days<=270:
+            return True
+    else: 
+        return False
+   
+
+def Check_Gender(fam_id,fam_dict,indi_dict):
+    husb_id = fam_dict[fam_id]['husb_id']
+    wife_id = fam_dict[fam_id]['wife_id']
+    
+    if indi_dict[husb_id]['gender']=="M" and indi_dict[wife_id]['gender']=="F":
+        return True
     else:
         return False
-
-
-        
+    
