784a89d2a53ad3e52a346889a451eb678791fb95
iamme1234567 <iamme930910@gmail.com>
2018-03-17 23:10:16 -0400

    Add files via upload

--- Sprint 2.py
+++ Sprint 2.py
@@ -4,32 +4,16 @@ Created on Thu Feb 22 18:19:21 2018
 
 @author: yujiezhong
 """
-import datetime
-
-
-def Marriage_Before_Death(marr,hdeath,wdeath):
-    #marriage date
-    marrdate = datetime.datetime.strptime(marr,"%Y-%m-%d")
-    #husband death date
-    if hdeath != "NA":
-        hdeathdate = datetime.datetime.strptime(hdeath,"%Y-%m-%d")
-    #wife death date
-    if wdeath != "NA":
-        wdeathdate = datetime.datetime.strptime(wdeath,"%Y-%m-%d")
     
-    if hdeath == "NA" and wdeath == "NA":
+    
+def No_Marriage_To_Descendants(fam_dict):
+    for key in fam_dict:
+        husb_id = fam_dict[key]['husb_id']
+        wife_id = fam_dict[key]['wife_id']
+        children = fam_dict[key]['children']
+        if husb_id in children or wife_id in children:
+            return False
         return True
-    elif hdeath == "NA" and wdeath != "NA":
-        if (wdeathdate - marrdate).days > 0:
-            return True
-    elif hdeath != "NA" and wdeath == "NA":
-        if (hdeathdate - marrdate).days > 0:
-            return True
-    elif hdeath != "NA" and wdeath != "NA":
-        if (wdeathdate - marrdate).days > 0 and (hdeathdate - marrdate).days > 0:
-            return True
-    else:
-        return False
         
 
 def Unique_Name_N_Birth(ID1,ID2,indi_dict):    
@@ -39,7 +23,7 @@ def Unique_Name_N_Birth(ID1,ID2,indi_dict):
     birt2 = indi_dict[ID2]['dob']
     
     if name1 == name2 and birt1 == birt2:
-        del indi_dict[ID2]
+        indi_dict.pop(ID2)
         return False
     else:
         return True
